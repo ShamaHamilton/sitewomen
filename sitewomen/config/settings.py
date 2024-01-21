b100382dc9eb2276ca187a3aae2695fd83a36ff2
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-01-21 21:31:46 +0300

    feat: Request exception handlers

--- sitewomen/config/settings.py
+++ sitewomen/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kwo=*@eg$7vulvq##5744g%lkm_mz(gno+e^98q(0qatu^xdp4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -15,4 +15,10 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2024:
+        raise Http404()
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
