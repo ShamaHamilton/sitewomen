7064050489784bc80f71ff2cde0880a015d4f9bf
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-02-13 12:35:21 +0300

    feat: Templates - tag url.

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -6,10 +6,10 @@ from women.converters import FourDigitYearConverter
 register_converter(FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('about/', about, name='about'),
-    path('cats/<int:cat_id>/', categories, name='cats_id'),
-    path('cats/<slug:cat_slug>/', categories_by_slug, name='cats'),
-    # re_path(r"^archive/(?P<year>[0-9]{4})/", archive),
-    path('archive/<year4:year>/', archive, name='archive'),
+    path('',                    index,      name='home'),
+    path('about/',              about,      name='about'),
+    path('addpage/',            addpage,    name='add_page'),
+    path('contact/',            contact,    name='contact'),
+    path('login/',              login,      name='login'),
+    path('post/<int:post_id>/', show_post,  name='post'),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -10,7 +10,24 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [
+    {
+        'title': 'О сайте',
+        'url_name': 'about'
+    },
+    {
+        'title': 'Добавить статью',
+        'url_name': 'add_page'
+    },
+    {
+        'title': 'Обратная связь',
+        'url_name': 'contact'
+    },
+    {
+        'title': 'Войти',
+        'url_name': 'login'
+    },
+]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
@@ -20,8 +37,6 @@ data_db = [
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
     data = {
         'title': 'Главная страница',
         'menu': menu,
@@ -34,27 +49,20 @@ def about(request: HttpRequest) -> HttpResponse:
     return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
-def categories(request: HttpRequest, cat_id: int) -> HttpResponse:
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+def show_post(request: HttpRequest, post_id: int) -> HttpResponse:
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
-def categories_by_slug(request: HttpRequest, cat_slug: str) -> HttpResponse:
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+def addpage(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Отображение статьи')
 
 
-def archive(request: HttpRequest, year: int) -> HttpResponse:
-    if year > 2024:
-        # raise Http404()
-        # return redirect('/', permanent=True)
-        # return redirect(index)
-        # return redirect('home')
-        # return redirect('cats', 'music')
-        # return HttpResponseRedirect(uri)
-        # return HttpResponsePermanentRedirect(uri)
+def contact(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Обратная связь')
 
-        uri = reverse('cats', args=('music', ))
-        return redirect(uri)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def login(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Авторизация')
 
 
 def page_not_found(request: HttpRequest, exception) -> HttpResponseNotFound:
