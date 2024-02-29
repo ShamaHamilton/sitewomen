511e5725fb8196b7e7b90723a4c5d8b59bbfb482
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-02-29 19:55:54 +0300

    feat: Validation form fields.

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -1,35 +1,75 @@
+from typing import Any
+
 from django import forms
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.utils.deconstruct import deconstructible
+from django.core.exceptions import ValidationError
+
 from .models import Category, Husband
+
+
+@deconstructible
+class RussianValidator:
+    # Если необходима общая проверка, которая используется многократно.
+    ALLOWED_CHARS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя0123456789- '
+    code = 'russian'
+
+    def __init__(self, message=None) -> None:
+        self.message = message if message else 'Должны присутствовать только русские символы, дефис и пробел.'
+
+    def __call__(self, value, *args: Any, **kwds: Any) -> Any:
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code)
 
 
 class AddPostForm(forms.Form):  # Форма, не связанная с моделью
     title = forms.CharField(
-        max_length=255,
         label='Заголовок',
-        widget=forms.TextInput(attrs={'class': 'form-input'})
+        max_length=255,
+        min_length=5,
+        widget=forms.TextInput(attrs={'class': 'form-input'}),
+        # validators=[
+        #     RussianValidator(),
+        # ],
+        error_messages={
+            'min_length': 'Слишком короткий заголовок',
+            'required': 'Без заголовка никак',
+        }
     )
     slug = forms.SlugField(
+        label='URL',
         max_length=255,
-        label='URL'
+        validators=[
+            MinLengthValidator(5, message='Минимум 5 символов'),
+            MaxLengthValidator(100, message='Максимум 100 символов'),
+        ],
     )
     content = forms.CharField(
+        label='Контент',
         widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}),
         required=False,
-        label='Контент'
     )
     is_published = forms.BooleanField(
+        label='Статус',
         required=False,
         initial=True,
-        label='Статус'
     )
     category = forms.ModelChoiceField(
+        label='Категории',
         queryset=Category.objects.all(),
         empty_label='Категория не выбрана',
-        label='Категории'
     )
     husband = forms.ModelChoiceField(
+        label='Муж',
         queryset=Husband.objects.all(),
         required=False,
         empty_label='Не замужем',
-        label='Муж'
     )
+
+    def clean_title(self):
+        # Если нужна частная проверка для определенного поля.
+        title = self.cleaned_data['title']
+        ALLOWED_CHARS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщьыъэюя0123456789- '
+
+        if not (set(title) <= set(ALLOWED_CHARS)):
+            raise ValidationError('Должны присутствовать только русские символы, дефис и пробел.')
--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -3,38 +3,73 @@ from transliterate import translit
 from django.db.models.query import QuerySet
 from django.urls import reverse
 from django.template.defaultfilters import slugify
-from django.db.models import (
-    Model, Index, Manager, IntegerChoices,
-    CharField, TextField, DateTimeField, BooleanField, SlugField, IntegerField,
-    ForeignKey, ManyToManyField, OneToOneField, PROTECT, SET_NULL
-)
+from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
-class PublishedManager(Manager):
+class PublishedManager(models.Manager):
     def get_queryset(self) -> QuerySet:
         return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
 
-class Women(Model):
-    class Status(IntegerChoices):
+class Women(models.Model):
+    class Status(models.IntegerChoices):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
 
-    title = CharField(max_length=255, verbose_name='Заголовок')
-    slug = SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug')
-    content = TextField(blank=True, verbose_name='Текст статьи')
-    time_create = DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    time_update = DateTimeField(auto_now=True, verbose_name='Время изменения')
-    is_published = BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
-                                default=Status.DRAFT,
-                                verbose_name='Статус')
-    category = ForeignKey('Category', on_delete=PROTECT, related_name='posts',
-                          verbose_name='Категории')  # women_set -> posts
-    tags = ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name='Теги')
-    husband = OneToOneField('Husband', on_delete=SET_NULL, null=True,
-                            blank=True, related_name='wuman', verbose_name='Муж')
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=255,
+    )
+    slug = models.SlugField(
+        verbose_name='Slug',
+        max_length=255,
+        unique=True,
+        db_index=True,
+        validators=[
+            MinLengthValidator(5, message='Минимум 5 символов'),
+            MaxLengthValidator(100, message='Максимум 100 символов'),
+        ],
+    )
+    content = models.TextField(
+        verbose_name='Текст статьи',
+        blank=True,
+    )
+    time_create = models.DateTimeField(
+        verbose_name='Время создания',
+        auto_now_add=True,
+    )
+    time_update = models.DateTimeField(
+        verbose_name='Время изменения',
+        auto_now=True,
+    )
+    is_published = models.BooleanField(
+        verbose_name='Статус',
+        choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+        default=Status.DRAFT,
+    )
+    category = models.ForeignKey(
+        verbose_name='Категории',
+        to='Category',
+        on_delete=models.PROTECT,
+        related_name='posts',  # women_set -> posts
+    )
+    tags = models.ManyToManyField(
+        verbose_name='Теги',
+        to='TagPost',
+        blank=True,
+        related_name='tags',
+    )
+    husband = models.OneToOneField(
+        verbose_name='Муж',
+        to='Husband',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='wuman',
+    )
 
-    objects = Manager()
+    objects = models.Manager()
     published = PublishedManager()
 
     def __str__(self) -> str:
@@ -46,7 +81,7 @@ class Women(Model):
 
         ordering = ['-time_create']
         indexes = [
-            Index(fields=['-time_create'])
+            models.Index(fields=['-time_create'])
         ]
 
     def get_absolute_url(self):
@@ -57,9 +92,17 @@ class Women(Model):
     #     super().save(*args, **kwargs)
 
 
-class Category(Model):
-    name = CharField(max_length=100, db_index=True, verbose_name='Категория')
-    slug = SlugField(max_length=255, unique=True, db_index=True)
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Категория',
+        max_length=100,
+        db_index=True,
+    )
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        db_index=True,
+    )
 
     class Meta:
         verbose_name = 'Категория'
@@ -72,9 +115,16 @@ class Category(Model):
         return reverse('category', kwargs={'cat_slug': self.slug})
 
 
-class TagPost(Model):
-    tag = CharField(max_length=100, db_index=True)
-    slug = SlugField(max_length=255, unique=True, db_index=True)
+class TagPost(models.Model):
+    tag = models.CharField(
+        max_length=100,
+        db_index=True,
+    )
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        db_index=True,
+    )
 
     def __str__(self) -> str:
         return self.tag
@@ -83,9 +133,13 @@ class TagPost(Model):
         return reverse('tag', kwargs={'tag_slug': self.slug})
 
 
-class Husband(Model):
-    name = CharField(max_length=100)
-    age = IntegerField(null=True)
+class Husband(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    age = models.IntegerField(
+        null=True
+    )
 
     def __str__(self) -> str:
         return self.name
