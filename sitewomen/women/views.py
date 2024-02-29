21389f88f301a8907df1df8905d2cfaa066de72a
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-02-29 19:07:31 +0300

    feat: Display form fields.

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -53,7 +53,12 @@ def addpage(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
     elif request.method == 'GET':
         form = AddPostForm()
 
